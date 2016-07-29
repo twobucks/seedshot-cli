801230d50097ae670086dab53f5aef2e4c0aff89
Hrvoje Šimić <shime@twobucks.co>
2016-07-29 16:53:31 +0200

    stop using shell commands

--- index.js
+++ index.js
@@ -1,19 +1,41 @@
-var spawn = require("child_process").exec
+var exec = require("child_process").execSync
 var platform = require('os').platform()
-var host = process.env.SEEDSHOT_HOST || "http://seedshot.io/"
+var path = require('path')
+var tmpdir = require('os').tmpdir()
+var request = require('request')
+var fs = require('fs')
+
+var host = process.env.SEEDSHOT_HOST || 'http://seedshot.io/'
+var screenshotPath = path.join(tmpdir, 'screenshot.jpg')
+var screenshotCommand = undefined
+var openCommand = undefined
 
 module.exports = function(){
   if (/darwin/.test(platform)){
-    spawn("screencapture -i screenshot.jpg && " +
-          "open $(node -pe '\"" + host + "\" " +
-          "+ JSON.parse(process.argv[1]).uuid' " +
-          "$(curl -F 'file=@screenshot.jpg' " + host + ")) " +
-          "&& rm screenshot.jpg")
+    screenshotCommand = "screencapture -i"
+    openCommand = "open"
   } else if (/freebsd/.test(platform) || /linux/.test(platform)) {
-    spawn("scrot -s screenshot.jpg && " +
-          "xdg-open $(node -pe '\"" + host + "\" " +
-          "+ JSON.parse(process.argv[1]).uuid' " +
-          "$(curl -F 'file=@screenshot.jpg' " + host + ")) " +
-          "&& rm screenshot.jpg")
+    screenshotCommand = "scrot -s"
+    openCommand = "xdg-open"
   }
+
+  // take the screenshot
+  exec(screenshotCommand + " " + screenshotPath)
+
+  // upload screenshot to the server
+  request.post({
+    url: host,
+    formData: { file: fs.createReadStream(screenshotPath)}
+  }, function(err, httpResponse, body){
+    if (err) {
+      return console.error('upload failed:', err)
+    }
+    var uuid =  JSON.parse(body).uuid
+
+    // open the browser
+    exec(openCommand + " " + path.join(host, uuid))
+
+    // remove the screenshot
+    fs.unlink(screenshotPath)
+  })
 }
